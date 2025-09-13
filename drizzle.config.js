7c0e79003d33e1c3f329550ab6c7362d0c118e55
harshithc31 <harshitlp2001@gmail.com>
2025-09-13 22:32:46 +0530

    Initial commit - AI Product Ads Generator

--- drizzle.config.js
+++ drizzle.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   schema: './utils/schema.jsx', // Change schema.ts to schema.js
   dialect: 'postgresql',
   dbCredentials: {
-    url: 'postgresql://neondb_owner:npg_P3RZok2zuyAU@ep-young-water-a11x0mld-pooler.ap-southeast-1.aws.neon.tech/Expense-Tracker?sslmode=require',
+    url: process.env.DATABASE_URL,
   },
 });
